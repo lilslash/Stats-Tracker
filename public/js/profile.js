92b3db75e8ed9676b95a5e3ba587d839df9bd509
Brad Lindsay <bslindsa@gmail.com>
2022-01-28 13:42:30 -0700

    creat battler stats

--- public/js/profile.js
+++ public/js/profile.js
@@ -4,17 +4,16 @@ const apexStatsHandler = async (event) => {
   // Collect values from stats form
   const kills = document.querySelector('#kills').value.trim();
   const wins = document.querySelector('#wins').value.trim();
-  const matchPlayed = document.querySelector('#matches-played').value.trim();
+  const matches_played = document.querySelector('#matches-played').value.trim();
 
   // Send post request to battle-royale endpoint
   const response = await fetch('/api/battle-royale', {
     method: 'POST',
-    body: JSON.stringify({ kills, wins, matchPlayed }),
+    body: JSON.stringify({ kills, wins, matches_played }),
     headers: { 'Content-Type': 'application/json' }
   });
 
   if (response.ok) {
-    console.log('It worked');
     document.location.replace('/profile');
   } else {
     alert('Failed to add stats');
