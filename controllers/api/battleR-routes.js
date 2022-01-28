273ea442a4de3d3b2ffc17ff7e1c64817b60b51e
Brad Lindsay <bslindsa@gmail.com>
2022-01-28 14:14:11 -0700

    fix bug

--- controllers/api/battleR-routes.js
+++ controllers/api/battleR-routes.js
@@ -3,34 +3,28 @@ const { BattleR, User } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 
-// Find all Duos stats
 router.get('/', async (req, res) => {
-    try {
-      const battleRoyaleData = await BattleR.findAll();
-      res.status(200).json(battleRoyaleData);
+  try {
+    const duosData = await BattleR.findAll();
+    res.status(200).json(duosData);
 
-router.get('/', async (req, res) => {
-    try {
-      const duosData = await BattleR.findAll();
-      res.status(200).json(duosData);
-
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // Create new battle royale stats
 router.post('/', withAuth, async (req, res) => {
-    try {
-      const battleRoyaleData = await BattleR.create({
-        ...req.body,
-        user_id: req.session.user_id
-      });
+  try {
+    const battleRoyaleData = await BattleR.create({
+      ...req.body,
+      user_id: req.session.user_id
+    });
 
-      res.status(200).json(battleRoyaleData);
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  });
+    res.status(200).json(battleRoyaleData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 module.exports = router;
