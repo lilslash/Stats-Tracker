6dc9f80d48c93cd4bbee0e47756d16db32bda188
Brad Lindsay <bslindsa@gmail.com>
2022-01-28 11:58:02 -0700

    update profile page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -27,7 +27,7 @@ router.get('/profile', withAuth, async (req, res) => {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ['password'] },
-      // include: [{ model: BattleR }],
+      // include: [{ model: BattleR }]
     });
 
     const user = userData.get({ plain: true });
