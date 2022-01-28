a39bb16559049ede6bc86c03a223bb7abfaa159a
lilslash <maxwittner01@gmail.com>
2022-01-28 14:28:27 -0700

    Escape

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,36 +3,17 @@ const { User, BattleR } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-<<<<<<< HEAD
     try {
         const battleData = await BattleR.findAll();
-=======
-  try {
-    const battleData = await BattleR.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
->>>>>>> main
 
         const battles = battleData.map((battle) => battle.get({plain: true}));
 
-<<<<<<< HEAD
         res.render('homepage'
         // , {
             // battles,
             // logged_in: req.session.logged_in
         // }
         );
-=======
-    res.render('homepage', {
-      battles,
-      logged_in: req.session.logged_in
-    });
->>>>>>> main
 
         console.log(battles);
     } catch (err) {
@@ -42,7 +23,6 @@ router.get('/', async (req, res) => {
 
 // Use withAuth middleware to prevent access to route
 router.get('/profile', withAuth, async (req, res) => {
-<<<<<<< HEAD
   try {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
@@ -60,25 +40,6 @@ router.get('/profile', withAuth, async (req, res) => {
     res.status(500).json(err);
   }
 });
-=======
-    try {
-      // Find the logged in user based on the session ID
-      const userData = await User.findByPk(req.session.user_id, {
-        attributes: { exclude: ['password'] },
-        include: [{ model: Project }],
-      });
-  
-      const user = userData.get({ plain: true });
-  
-      res.render('profile', {
-        ...user,
-        logged_in: true
-      });
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
->>>>>>> parent of ded90ec (login functioning)
 
 router.get('/login', (req, res) => {
     // If the user is already logged in, redirect the request to another route
